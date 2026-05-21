dc232c9f397b95d1a13645999311a60e7c33a8be
Kong Hyunjoo <kjenny9701@naver.com>
2026-05-22 08:15:26 +0900

    fix: 게시글 조회 및 작성 URL 경로를 /posts/로 수정

--- SplitIt/urls.py
+++ SplitIt/urls.py
@@ -9,10 +9,10 @@ app_name = 'SplitIt'
 
 urlpatterns = [
 
- path('', PostListView.as_view()),
- path('<int:pk>/', PostDetailView.as_view()),
- path('comments/', CommentView.as_view()),
- path('', PostListView.as_view(), name='post_list'),
+ path('posts/', PostListView.as_view(), name='post_list'),
+ path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+ path('posts/comments/', CommentView.as_view(), name='comments'),
+ 
  path('my-posts/', MyPostListView.as_view(), name='my_posts'),
  path('matching-history/', MyMatchingHistoryView.as_view(), name='matching_history'),
  path('<int:post_id>/join/', JoinPostView.as_view(), name='join_post'),
